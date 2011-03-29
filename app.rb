a7fb71e6191bcb4635cd315868b214eec0bd648e
scottmotte <scott@scottmotte.com>
2011-03-28 20:34:19 -0700

    set the time with @rmeetup - thanks meng

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'rmeetup'
+
+before do
+  RMeetup::Client.api_key = "6b47764419734b346f14506a444f302a"
+  @rmeetup = RMeetup::Client.fetch(:events, {:group_urlname => "riversideruby",:before => '1m'}).first
+end
 
 get '/' do
   haml :index
